970e126d25847d599a0538f111c7bb1854230a52
Lahari Rao | Java Developer <laharirao2106@gmail.com>
2026-08-20 18:21:06 +0530

    Time: 1 ms (98.08%) | Memory: 46.6 MB (81.25%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.java
@@ -3,22 +3,14 @@ class Solution {
 
         int n = nums.length;
 
-        int[] arr1 = new int[n];
-        int[] arr2 = new int[n];
+        int[] arr1 = new int[n]; int m = 0; 
+        int[] arr2 = new int[n]; int k = 0; 
 
-        int m = 0; // size of arr1
-        int k = 0; // size of arr2
-
-        // First operation
         arr1[m++] = nums[0];
-
-        // Second operation
         arr2[k++] = nums[1];
 
-        // Remaining elements
         for (int i = 2; i < n; i++) {
 
-            // Compare ONLY the last elements
             if (arr1[m - 1] > arr2[k - 1]) {
                 arr1[m++] = nums[i];
             } else {
@@ -30,17 +22,14 @@ class Solution {
         int[] result = new int[n];
 
         int index = 0;
-
-        // Add arr1
-        for (int i = 0; i < m; i++) {
+    
+            for (int i = 0; i < m; i++) {
             result[index++] = arr1[i];
         }
 
-        // Add arr2
         for (int i = 0; i < k; i++) {
             result[index++] = arr2[i];
         }
-
         return result;
     }
 }
