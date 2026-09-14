f7bce980f6623304565ace482e44014d7ec849b5
Lahari Rao | Java Developer <laharirao2106@gmail.com>
2026-09-14 19:22:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (17.64%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] a, int[] b) {
+
+        int left1 = a[0];
+        int bottom1 = a[1];
+        int right1 = a[2];
+        int top1 = a[3];
+
+        int left2 = b[0];
+        int bottom2 = b[1];
+        int right2 = b[2];
+        int top2 = b[3];
+
+        // Check if they overlap horizontally
+        boolean horizontal = left1 < right2 && left2 < right1;
+
+        // Check if they overlap vertically
+        boolean vertical = bottom1 < top2 && bottom2 < top1;
+
+        // Both horizontal and vertical overlap are required
+        return horizontal && vertical;
+    }
+}
