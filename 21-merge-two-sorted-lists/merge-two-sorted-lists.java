a4456bb53879ad75d9c33e93b49505038fceb680
Lahari Rao | Java Developer <laharirao2106@gmail.com>
2026-09-18 19:43:08 +0530

    Add swapPairs method to Solution class
    Implemented a method to swap pairs of nodes in a linked list.

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,4 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
 
+        while (current.next != null && current.next.next != null) {
+           ListNode first = current.next;
+            ListNode second = current.next.next;
+        
+            first.next = second.next;
+            second.next = first;
+            current.next = second;
+            current = first;
+        }
+
+        return dummy.next;
+    }
+}
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         // Create a dummy node to start our new merged list
