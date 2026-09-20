3ce8cb8769d5d9325e4c98a68616078dfb51cc69
Lahari Rao | Java Developer <laharirao2106@gmail.com>
2026-09-20 18:08:31 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (34.29%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,46 +1,9 @@
 class Solution {
-
-    // Function to get reverse alphabet value
-    int getValue(char c) {
-        switch (c) {
-            case 'a': return 26;
-            case 'b': return 25;
-            case 'c': return 24;
-            case 'd': return 23;
-            case 'e': return 22;
-            case 'f': return 21;
-            case 'g': return 20;
-            case 'h': return 19;
-            case 'i': return 18;
-            case 'j': return 17;
-            case 'k': return 16;
-            case 'l': return 15;
-            case 'm': return 14;
-            case 'n': return 13;
-            case 'o': return 12;
-            case 'p': return 11;
-            case 'q': return 10;
-            case 'r': return 9;
-            case 's': return 8;
-            case 't': return 7;
-            case 'u': return 6;
-            case 'v': return 5;
-            case 'w': return 4;
-            case 'x': return 3;
-            case 'y': return 2;
-            case 'z': return 1;
-            default: return 0;
-        }
-    }
-
     public int reverseDegree(String s) {
-
         int sum = 0;
 
         for (int i = 0; i < s.length(); i++) {
-
-            int value = getValue(s.charAt(i));
-
+            int value = 'z' - s.charAt(i) + 1;
             sum += value * (i + 1);
         }
 
